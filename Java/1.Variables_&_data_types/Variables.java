f7f85e29bdd2e70c67654c315c465f0e46f53ccf
rahul-roy28 <rahulroy36464@gmail.com>
2024-07-04 18:42:29 +0530

    Variables in java

---
+++ Java/1.Variables_&_data_types/Variables.java
@@ -0,0 +1,10 @@
+public class Variables{
+ public static void main(String args[]){
+  int a=10;
+  int b=20;
+  String name="Rahul Roy";
+  System.out.println(a);
+  System.out.println(b);
+  System.out.print(name);
+ }
+}
