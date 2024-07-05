c368df78ac75f2f59c48a4536e8c2f1452cb1677
rahul-roy28 <rahulroy36464@gmail.com>
2024-07-05 13:45:11 +0530

    Type Promotion in Experssions

---
+++ Java/1.Variables_&_data_types/TypePromotion.java
@@ -0,0 +1,14 @@
+public class TypePromotion{
+ public static void main(String args[]){
+  // byte a=24;
+  // short b=20;
+  // char c='c';
+  // int sum=a+b+c;
+
+  long a=23;
+  float b=24.75f;
+  double c=20;
+  double sum=a+b+c;
+  System.out.println(sum);
+ }
+}
