0bf11127306563595976db51eff353e8e977a37f
rahul-roy28 <rahulroy36464@gmail.com>
2024-07-05 20:37:49 +0530

    Solve Assignment

---
+++ Java/2.Operators/Assignment.java
@@ -0,0 +1,18 @@
+// public class Assignment{
+//  public static void main(String args[]){
+//   int x = 2, y = 5;
+//   int exp1 = (x * y / x);
+//   int exp2 = (x * (y / x));
+//   System.out.print(exp1);
+//   System.out.print(exp2);
+//  }
+// }
+
+public class Assignment {
+ public static void main(String[] args){
+  int x = 9, y = 12;
+  int a = 2, b = 4, c = 6;
+  int exp = 4/3 * (x + 34) + 9 * (a + b * c) + (3 + y * (2 + a)) / (a + b*y);
+  System.out.println(exp);
+  }
+ }
