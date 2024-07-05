4eb01969c9bcd237ae085ce15a2819e93d4d0bcd
rahul-roy28 <rahulroy36464@gmail.com>
2024-07-05 13:15:01 +0530

    Type casting

---
+++ Java/1.Variables_&_data_types/TypeCasting.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class TypeCasting{
+ public static void main(String args[]){
+  Scanner sc=new Scanner(System.in);
+  //char to int casting
+  char ch='P';
+  int no=ch;
+  System.out.println(no);
+  char ch1='u';
+  int no1=ch1;
+  System.out.println(no1);
+  char ch2='j';
+  int no2=ch2;
+  System.out.println(no2);
+  char ch3='a';
+  int no3=ch3;
+  System.out.println(no3);
+  //float to int
+  float a=22.56f;
+  int b=(int)a;
+  System.out.println(b);
+ }
+}
