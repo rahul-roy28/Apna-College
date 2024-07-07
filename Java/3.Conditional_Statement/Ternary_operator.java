2374a13183ead8922ac6d721fef978c0d0cf2654
rahul-roy28 <rahulroy36464@gmail.com>
2024-07-08 01:31:03 +0530

    Ternary operator

---
+++ Java/3.Conditional_Statement/Ternary_operator.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Ternary_operator{
+ public static void main(String args[]){
+  int n=21;
+   String type=((n%2)==0) ? "Even":"Odd";
+   System.out.println(type);
+ }
+}
