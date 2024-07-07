c6158cb313e2c4cc2ffe1d8b9109ba9942c1a40d
rahul-roy28 <rahulroy36464@gmail.com>
2024-07-07 21:42:14 +0530

    Check largest number

---
+++ Java/3.Conditional_Statement/largestNumber.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class largestNumber{
+ public static void main(String args[]){
+  int a=2;
+  int b=4;
+  if(a>b){
+   System.out.println("a is largest");
+  }
+  else{
+   System.out.println("a is largest");
+  }
+ }
+}
