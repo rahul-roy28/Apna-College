2e85044c8f4f6925c5f9787d6db4d2324c4a906a
rahul-roy28 <rahulroy36464@gmail.com>
2024-07-08 01:38:39 +0530

    pass fail check using ternary operator

---
+++ Java/3.Conditional_Statement/pass_fail.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class pass_fail{
+ public static void main(String args[]){
+  int marks=60;
+   String type=(marks>=33)? "Pass":"Fail";
+   System.out.println(type);
+ }
+}
