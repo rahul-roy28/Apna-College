24f0def1110090841c56c1fac8d110946c08cf57
rahul-roy28 <rahulroy36464@gmail.com>
2024-07-07 22:42:49 +0530

    Else if condition

---
+++ Java/3.Conditional_Statement/else_if.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class if_else{
+ public static void main(String args[]){
+  int age=22;
+  if(age>=18){
+   System.out.println("Adult:Drive,vote");
+  }//if this condition is true then else if and else condition does not compile. if thecondition is false then else if and else condition will compile. 
+  else if(age>16 && age<18){
+   System.out.println("Teenager");
+  }
+  else{
+   System.out.println("Not Adult");
+  }
+ }
+}
