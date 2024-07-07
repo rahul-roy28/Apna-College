4930fdbfb1972cff81ff3e9624f0a0247082c05a
rahul-roy28 <rahulroy36464@gmail.com>
2024-07-07 21:33:57 +0530

    If Else condition

---
+++ Java/3.Conditional_Statement/if_else.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class if_else{
+ public static void main(String args[]){
+  int age=22;
+  if(age>=18){
+   System.out.println("Adult:Drive,vote");
+  }
+  if(age>16 && age<18){
+   System.out.println("Teenager");
+  }
+  else{
+   System.out.println("Not Adult");
+  }
+ }
+}
