3629fc73d1ddf6bcf2ec8092ca4c6c2b081f0ae9
rahul-roy28 <rahulroy36464@gmail.com>
2024-07-11 21:59:58 +0530

    break statement

---
+++ Java/4.Loops/break_statement.java
@@ -0,0 +1,12 @@
+public class break_statement {
+ public static void main(String[] args) {
+  for(int i=0;i<=4;i++){
+   if(i==3){
+    break;
+   }
+   System.out.print(i+" ");
+  }
+  System.out.println();
+  System.out.print("I am out of the loop");
+ }
+}
