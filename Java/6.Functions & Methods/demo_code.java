cb2b51f8f2e846909a60b9a0b0321f23ed1d77e4
rahul-roy28 <rahulroy36464@gmail.com>
2024-07-14 17:08:00 +0530

    execute function demo code

---
+++ Java/6.Functions & Methods/demo_code.java
@@ -0,0 +1,12 @@
+public class demo_code {
+ public static void printHelloWorld() {
+  System.out.println("Hello World");
+  System.out.println("Hello World");
+  System.out.println("Hello World");
+  System.out.println("Hello World");
+  System.out.println("Hello World");
+ }
+ public static void main(String[] args) {
+  printHelloWorld();//Function call
+ }
+}
