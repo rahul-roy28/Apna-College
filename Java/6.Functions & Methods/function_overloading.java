4afde21b30d954e61d1d5390ee908a453934c26a
rahul-roy28 <rahulroy36464@gmail.com>
2024-07-16 01:03:01 +0530

    Function overloading using Parameters

---
+++ Java/6.Functions & Methods/function_overloading.java
@@ -0,0 +1,12 @@
+public class function_overloading {
+ public static int sum(int a,int b) {
+  return a+b;
+ }
+ public static int sum(int a,int b,int c) {
+  return a+b+c;
+ }
+ public static void main(String[] args) {
+  System.out.println(sum(2,5));
+  System.out.println(sum(3,4,6));
+ }
+}
