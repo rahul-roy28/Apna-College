51844a21766b84d7dd62cde88f354015012155bd
rahul-roy28 <rahulroy36464@gmail.com>
2024-07-16 01:10:19 +0530

    Function overloading using Data types

--- Java/6.Functions & Methods/function_overloading.java
+++ Java/6.Functions & Methods/function_overloading.java
@@ -1,12 +1,22 @@
 public class function_overloading {
+ 
+ // Using Parameters
+ // public static int sum(int a,int b) {
+ //  return a+b;
+ // }
+ // public static int sum(int a,int b,int c) {
+ //  return a+b+c;
+ // }
+ 
+ // Using Data types
  public static int sum(int a,int b) {
   return a+b;
  }
- public static int sum(int a,int b,int c) {
-  return a+b+c;
+ public static float sum(float a,float b) {
+  return a+b;
  }
  public static void main(String[] args) {
   System.out.println(sum(2,5));
-  System.out.println(sum(3,4,6));
+  System.out.println(sum(5.7f,2.3f));
  }
 }
