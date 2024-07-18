504154950b2757e2f2d80fd6edaf307d3881bea1
rahul-roy28 <rahulroy36464@gmail.com>
2024-07-18 17:06:22 +0530

    print zero one triangle pattern

---
+++ Java/7.Pattern print(part 2)/zero_one_triangle.java
@@ -0,0 +1,17 @@
+public class zero_one_triangle {
+ public static void zero_one(int n) {
+  for(int i=1;i<=n;i++){
+   for(int j=1;j<=i;j++){
+    if((i+j)%2==0){
+     System.out.print("1 ");
+    }else{
+     System.out.print("0 ");
+    }
+   }
+   System.out.println();
+  }
+ }
+ public static void main(String[] args) {
+  zero_one(8);
+ }
+}
