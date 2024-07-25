ce5904c8e7f9d05933aaf81674b582728b12fca8
rahul-roy28 <rahulroy36464@gmail.com>
2024-07-26 00:11:40 +0530

    .....

--- Java/9.Arrays(Part 2)/tapping_rainwater.java
+++ Java/9.Arrays(Part 2)/tapping_rainwater.java
@@ -24,7 +24,7 @@ public class tapping_rainwater {
   return trappedwater;
  }
  public static void main(String[] args) {
-  int height[]={4,2,0,6,3,2,5};
+  int height[]={4,2,0,3,2,5};
   System.out.println(trappedRainWater(height));
  }
 }
