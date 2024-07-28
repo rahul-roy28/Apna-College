8552cb6e395b29891f78834d8f3caebc8e9b4b35
rahul-roy28 <rahulroy36464@gmail.com>
2024-07-29 00:30:10 +0530

    Search in Sorted matrix using Staircase Search

---
+++ Java/11.2D_Arrays/Staircase_search.java
@@ -0,0 +1,28 @@
+public class Staircase_search {
+ public static boolean staircase(int matrix[][], int key) {
+  int row=0, col=matrix[0].length-1;
+  while (row<matrix.length && col>=0) {
+   if(matrix[row][col]==key){
+    System.out.println("Key found at ("+row+","+col+")");
+    return true;
+   }
+   else if(key<matrix[row][col]){
+    col--;
+   }
+   else{
+    row++;
+   }
+  }
+  System.out.println("Key not found");
+  return false;
+ }
+ public static void main(String[] args) {
+  int matrix[][]={{10,20,30,40},
+                  {15,25,35,45},
+                  {27,29,37,48},
+                  {32,33,39,50}
+                 };
+  int key=33;
+  staircase(matrix, key);
+ }
+}
