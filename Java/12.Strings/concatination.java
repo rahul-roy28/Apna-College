eadf4966be03ddf57f17f7e758e0455bf956ca65
rahul-roy28 <rahulroy36464@gmail.com>
2024-07-29 12:53:14 +0530

    Concatination in Strings

---
+++ Java/12.Strings/concatination.java
@@ -0,0 +1,8 @@
+public class concatination {
+ public static void main(String[] args) {
+  String firstname="Rahul";
+  String lastname="Roy";
+  String fullname=firstname+" "+lastname;
+  System.out.println(fullname);
+ }
+}
