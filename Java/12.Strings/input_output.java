07d9e05f61098fd7cd8064c6be277690dd87be7b
rahul-roy28 <rahulroy36464@gmail.com>
2024-07-29 12:46:21 +0530

    Input output in Strings

---
+++ Java/12.Strings/input_output.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class input_output {
+ public static void main(String[] args) {
+  Scanner sc=new Scanner(System.in);
+  String name=sc.nextLine();
+  System.out.println(name);
+  sc.close();
+ }
+}
