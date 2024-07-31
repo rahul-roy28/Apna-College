42328191ddca64102c518f2704974ee37a316521
rahul-roy28 <rahulroy36464@gmail.com>
2024-07-31 21:29:04 +0530

    String Compression

---
+++ Java/12.Strings/String_Compression.java
@@ -0,0 +1,21 @@
+public class String_Compression {
+ public static String compression(String str) {
+  String newStr="";
+  for(int i=0;i<str.length();i++){
+   Integer count=0;
+   while(i<str.length()-1 && str.charAt(i)==str.charAt(i+1)){
+    count++;
+    i++;
+   }
+   newStr +=str.charAt(i);
+   if(count>1){
+    newStr +=count.toString();
+   }
+  }
+  return newStr;
+ }
+ public static void main(String[] args) {
+  String str="aaabbbbcccccdddddd";
+  System.out.println(compression(str));
+ }
+}
