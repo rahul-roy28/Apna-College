fa7dc96d22784f94d9c46aaa8086ccbc8f04df00
rahul-roy28 <rahulroy36464@gmail.com>
2024-08-22 00:48:57 +0530

    Polymorphism, Method Overloading, Method Overriding

---
+++ Java/14.OOPs/polymorphism.java
@@ -0,0 +1,35 @@
+public class polymorphism {
+  public static void main(String[] args) {
+    // Method Overloading
+    Calculaor calc=new Calculaor();
+    System.out.println(calc.sum(3, 5));
+    System.out.println(calc.sum((float)1.5, (float)2.5));
+    System.out.println(calc.sum(4, 6, 8));
+    // Method Overriding
+    Deer d=new Deer();
+    d.eat();
+  }
+}
+// Method Overloading
+class Calculaor{
+  int sum(int a,int b){
+    return a+b;
+  }
+  float sum(float a,float b){
+    return a+b;
+  }
+  int sum(int a,int b,int c){
+    return a+b+c;
+  }
+}
+// Method Overriding
+class Animal{
+  void eat(){
+    System.out.println("Eats Anything");
+  }
+}
+class Deer extends Animal{
+  void eat(){
+    System.out.println("Eats Grass");
+  }
+}
