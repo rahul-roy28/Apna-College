5c74033af70ec8faf7050e7f03a6e463d67c6601
rahul-roy28 <rahulroy36464@gmail.com>
2024-09-16 11:57:45 +0530

    [Recursion Basics]:Print numbers from 1 ti n(Increasing order)

--- Java/15.Recursion(part 1)/printNumber.java
+++ Java/15.Recursion(part 1)/printNumber.java
@@ -2,7 +2,7 @@
  * printNumber
  */
 public class printNumber {
-
+  //Print number in decresing order
   public static void printDec(int n) {
     if(n==1){
       System.out.println(1);
@@ -11,8 +11,20 @@ public class printNumber {
     System.out.print(n+" ");
     printDec(n-1);
   }
+  //print number in increasing order
+  public static void printInc(int n) {
+    if(n==1){
+      System.out.print(1);
+      return;
+    }
+    printInc(n-1);
+    System.out.print(" "+n);
+    
+  }
   public static void main(String[] args) {
     int n=10;
+    printInc(n);
+    System.out.println();
     printDec(n);
   }
 }
