242704f50dccb7ca348d2aafdf3eb6efab5aecde
rahul-roy28 <rahulroy36464@gmail.com>
2024-09-19 16:50:44 +0530

    Add error handling process

--- Web Development/5Bakend/MONGO3/index.js
+++ Web Development/5Bakend/MONGO3/index.js
@@ -4,6 +4,7 @@ const path = require("path");
 const mongoose = require("mongoose");
 const Chat = require("./models/chat.js");
 const methodOverride = require("method-override");
+const ExpressError = require("./ExpressError");
 
 // Connection with mongoDB
 main()
@@ -25,19 +26,28 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
 // Index Route
-app.get("/chats", async (req, res) => {
-  let chats = await Chat.find(); //This is a Asyncronus Function
-  // console.log(chats);
-  res.render("index.ejs", { chats });
-});
+app.get(
+  "/chats",
+  asyncWrap(async (req, res) => {
+    // try {
+    let chats = await Chat.find(); //This is a Asyncronus Function
+    // console.log(chats);
+    res.render("index.ejs", { chats });
+    // } catch (err) {
+    // next(err);
+    // }
+  })
+);
 
 //New Route
 app.get("/chats/new", (req, res) => {
+  // throw new ExpressError(404, "Chat not found");
   res.render("new.ejs");
 });
 
 // Create Route
 app.post("/chats", (req, res) => {
+  // try {
   let { from, to, msg } = req.body;
   let newChat = new Chat({
     from: from,
@@ -54,41 +64,94 @@ app.post("/chats", (req, res) => {
       console.log(err);
     });
   res.redirect("/chats");
+  // } catch (err) {
+  //   next(err);
+  // }
 });
+// Use wrapAsync function for error handling
+function asyncWrap(fn) {
+  return function (req, res, next) {
+    fn(req, res, next).catch((err) => next(err));
+  };
+}
+
+// New -Show Route
+// Edit Route
+app.get(
+  "/chats/:id",
+  asyncWrap(async (req, res, next) => {
+    // try { (Using try and catch method for error handling)
+    let { id } = req.params;
+    let chat_id = await Chat.findById(id);
+    if (!chat_id) {
+      return next(new ExpressError(404, "Chat not found or Delete"));
+    }
+    res.render("edit.ejs", { chat_id });
+    // } catch (err) {
+    //   next(err);
+    // }
+  })
+);
 
 // Edit Route
-app.get("/chats/:id/edit", async (req, res) => {
-  let { id } = req.params;
-  let chat_id = await Chat.findById(id);
-  res.render("edit.ejs", { chat_id });
-});
+app.get(
+  "/chats/:id/edit",
+  asyncWrap(async (req, res) => {
+    // try {
+    let { id } = req.params;
+    let chat_id = await Chat.findById(id);
+    res.render("edit.ejs", { chat_id });
+    // } catch (err) {
+    //   next(err);
+    // }
+  })
+);
 
 // Update Route
-app.put("/chats/:id", async (req, res) => {
-  let { id } = req.params;
-  let { msg: newMsg } = req.body;
-  let updatedChat = await Chat.findByIdAndUpdate(
-    id,
-    { msg: newMsg },
-    { runValidators: true },
-    { new: true }
-  );
-  console.log(updatedChat);
-  res.redirect("/chats");
-});
+app.put(
+  "/chats/:id",
+  asyncWrap(async (req, res) => {
+    // try {
+    let { id } = req.params;
+    let { msg: newMsg } = req.body;
+    let updatedChat = await Chat.findByIdAndUpdate(
+      id,
+      { msg: newMsg },
+      { runValidators: true },
+      { new: true }
+    );
+    console.log(updatedChat);
+    res.redirect("/chats");
+    // } catch (err) {
+    //   next(err);
+    // }
+  })
+);
 
 // Delete Route
-app.delete("/chats/:id", async (req, res) => {
-  let { id } = req.params;
-  let deletedChat = await Chat.findByIdAndDelete(id);
-  console.log(deletedChat);
-  res.redirect("/chats");
-});
+app.delete(
+  "/chats/:id",
+  asyncWrap(async (req, res) => {
+    // try {
+    let { id } = req.params;
+    let deletedChat = await Chat.findByIdAndDelete(id);
+    console.log(deletedChat);
+    res.redirect("/chats");
+    // } catch (err) {
+    //   next(err);
+    // }
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Working root");
 });
 
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+  let { status = 500, message = "Some Error Occurred" } = err;
+  res.status(status).send(message);
+});
 app.listen(8080, () => {
   console.log("server is listening on port 8080");
 });
