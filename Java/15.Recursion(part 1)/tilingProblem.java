0b975453821b667a4d65f9df336bbf092c93f742
rahul-roy28 <rahulroy36464@gmail.com>
2025-01-21 23:56:04 +0530

    Add Problem statement

--- Java/15.Recursion(part 1)/tilingProblem.java
+++ Java/15.Recursion(part 1)/tilingProblem.java
@@ -1,3 +1,6 @@
+/*  Problem Statement
+You are given a 2xn board and an infinite number of 2x1 tiles. Your task is to write a function that calculates the number of ways to tile the entire 2xn board using these 2x1 tiles. The tiles can be placed either vertically or horizontally.
+*/
 public class tilingProblem {
  public static int tiling(int n) {
   if(n==0||n==1){
