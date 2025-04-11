70ab69a71e1d747d00351e4b59c0096f24a0d886
rahul-roy28 <rahulroy36464@gmail.com>
2025-04-11 19:40:22 +0530

    Build LL using java collection framework

---
+++ Java/22.LinkedList(Part 2)/framework.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class framework {
+  public static void main(String[] args) {
+    LinkedList<Integer> ll=new LinkedList<>();
+    ll.addFirst(1);
+    ll.addFirst(2);
+    ll.addFirst(3);
+    ll.addFirst(4);
+    ll.addFirst(5);
+    System.out.println(ll);
+  }
+}
