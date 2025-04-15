14ecc0e2849d028478ea06470a5296a137d971da
rahul-roy28 <rahulroy36464@gmail.com>
2025-04-15 21:42:47 +0530

    Push Data into Bottom of the stack

---
+++ Java/23.Stack(Part 1)/StackD.java
@@ -0,0 +1,24 @@
+// Push Data into Bottom of the stack
+import java.util.*;
+public class StackD {
+  public static void pushAtBottom(Stack<Integer> s,int data){
+    if(s.isEmpty()){
+      s.push(data);
+      return;
+    }
+    int top=s.pop();
+    pushAtBottom(s, data);
+    s.push(top);
+  }
+  public static void main(String[] args) {
+    Stack<Integer> s=new Stack<>();
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    pushAtBottom(s, 5);
+    while (!s.isEmpty()) {
+      System.out.println(s.pop());
+    }
+  }
+}
