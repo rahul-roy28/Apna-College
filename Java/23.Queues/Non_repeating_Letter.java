04f874c262001e4beef76f31f01d860b66fc1365
rahul-roy28 <rahulroy36464@gmail.com>
2025-04-20 12:29:24 +0530

    Added Java program to find first non-repeating character in a stream using queue

---
+++ Java/23.Queues/Non_repeating_Letter.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.util.LinkedList;
+public class Non_repeating_Letter {
+  public static void repeate(String str){
+    int freq[]=new int[26];
+    Queue<Character> q=new LinkedList<>();
+    for(int i=0;i<str.length();i++){
+      char ch=str.charAt(i);
+      q.add(ch);
+      freq[ch-'a']++;
+      while(!q.isEmpty() && freq[q.peek()-'a']>1){
+        q.remove();
+      }
+      if(q.isEmpty()){
+        System.out.print(-1+" ");
+      }else{
+        System.out.print(q.peek()+" ");
+      }
+    }
+    System.out.println();
+  }
+  public static void main(String[] args) {
+    String str="aabccxb";
+    repeate(str);
+  }
+}
