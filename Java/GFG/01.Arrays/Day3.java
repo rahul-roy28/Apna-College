eed97b50c620a846e8b28f81d244d72bbe6f6bd4
rahul-roy28 <rahulroy36464@gmail.com>
2025-04-22 17:21:00 +0530

    Java Program to reverse an array using Two Pointers

---
+++ Java/GFG/01.Arrays/Day3.java
@@ -0,0 +1,53 @@
+/*
+ * Given an array arr[], the task is to reverse the array. Reversing an array means rearranging the elements such that the first element becomes the last, the second element becomes second last and so on.
+
+Examples:
+
+Input: arr[] = {1, 4, 3, 2, 6, 5}
+Output: {5, 6, 2, 3, 4, 1}
+Explanation: The first element 1 moves to last position, the second element 4 moves to second-last and so on.
+
+Input: arr[] = {4, 5, 1, 2}
+Output: {2, 1, 5, 4}
+Explanation: The first element 4 moves to last position, the second element 5 moves to second last and so on.
+ */
+
+
+
+// Java Program to reverse an array using Two Pointers
+
+import java.util.Arrays;
+
+class Day3 {
+    
+    // function to reverse an array
+    static void reverseArray(int[] arr) {
+        
+        // Initialize left to the beginning and right to the end
+        int left = 0, right = arr.length - 1;
+
+        // Iterate till left is less than right
+        while (left < right) {
+            
+            // Swap the elements at left and right position
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            // Increment the left pointer
+            left++;
+
+            // Decrement the right pointer
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 4, 3, 2, 6, 5 };
+
+        reverseArray(arr);
+
+        for (int i = 0; i < arr.length; i++) 
+            System.out.print(arr[i] + " ");
+    }
+}
