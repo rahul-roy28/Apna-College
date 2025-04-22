b1491b11a19e55ae5ce6d4ef1fef3476b514ad34
rahul-roy28 <rahulroy36464@gmail.com>
2025-04-22 17:11:27 +0530

    Second Largest element in an array

---
+++ Java/GFG/01.Arrays/Day1.java
@@ -0,0 +1,50 @@
+/*Given an array of positive integers arr[] of size n, the task is to find second largest distinct element in the array.
+
+Note: If the second largest element does not exist, return -1.
+
+Examples:
+
+Input: arr[] = [12, 35, 1, 10, 34, 1]
+Output: 34
+Explanation: The largest element of the array is 35 and the second largest element is 34.
+
+Input: arr[] = [10, 5, 10]
+Output: 5
+Explanation: The largest element of the array is 10 and the second largest element is 5.
+
+Input: arr[] = [10, 10, 10]
+Output: -1
+Explanation: The largest element of the array is 10 there is no second largest element. */
+// Java program to find second largest element in an array
+// using Sorting
+
+import java.util.Arrays;
+
+class Day1 {
+    
+    // function to find the second largest element
+    static int getSecondLargest(int[] arr) {
+        int n = arr.length;
+        
+        // Sort the array in non-decreasing order
+        Arrays.sort(arr);
+        
+        // start from second last element as last element is the largest
+        for (int i = n - 2; i >= 0; i--) {
+            
+            // return the first element which is not equal to the 
+            // largest element
+            if (arr[i] != arr[n - 1]) {
+                return arr[i];
+            }
+        }
+        
+        // If no second largest element was found, return -1
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 12, 35, 1, 10, 34, 1 };
+        System.out.println(getSecondLargest(arr));
+    }
+}
