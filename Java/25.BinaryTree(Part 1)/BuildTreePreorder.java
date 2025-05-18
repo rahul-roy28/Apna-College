3d89c136f5d27c2b131586091dedb04ac5e73300
rahul-roy28 <rahulroy36464@gmail.com>
2025-05-18 12:51:20 +0530

    Perforn Preorder Traversal Function

--- Java/25.BinaryTree(Part 1)/BuildTreePreorder.java
+++ Java/25.BinaryTree(Part 1)/BuildTreePreorder.java
@@ -28,11 +28,19 @@ public class BuildTreePreorder {
       
       return newNode;
     }
+    public static void preorder(Node root){
+      if(root==null){
+        return;
+      }
+      System.out.print(root.data+" ");
+      preorder(root.left);
+      preorder(root.right);
+    }
   }
   public static void main(String[] args) {
     int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
     BinaryTree tree=new BinaryTree();
     Node root=tree.buildTree(nodes);
-    System.out.println(root.data);
+    tree.preorder(root);
   }
 }
