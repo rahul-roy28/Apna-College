72e078b9b951e0635f11b8d99831080a8c43436f
rahul-roy28 <rahulroy36464@gmail.com>
2025-05-19 23:54:34 +0530

    Compute diameter of a binary tree using recursive height-based approach

---
+++ Java/26.BinaryTree(Part 2)/Diameter.java
@@ -0,0 +1,51 @@
+import java.util.*;
+public class Diameter {
+  static class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+      this.data=data;
+      this.left=null;
+      this.right=null;
+    }
+  }
+  public static int height(Node root){
+      if(root==null){
+        return 0;
+      }
+      int lh=height(root.left);
+      int rh=height(root.right);
+      return Math.max(lh, rh)+1;
+  }
+  public static int diameter(Node root){
+    if(root==null){
+      return 0;
+    }
+    int leftDiam=diameter(root.left);
+    int leftHt=height(root.left);
+    int rightDiam=diameter(root.right);
+    int rightHt=height(root.right);
+    
+    int selfDiam=leftHt+rightHt+1;
+
+    return Math.max(selfDiam, Math.max(leftDiam,rightDiam));
+  }
+  public static void main(String[] args) {
+    /*
+                 1
+                / \
+               2   3
+             /  \ /  \
+            4   56   7
+     */
+    Node root=new Node(1);
+    root.left=new Node(2);
+    root.right=new Node(3);
+    root.left.left=new Node(4);
+    root.left.right=new Node(5);
+    root.right.left=new Node(6);
+    root.right.right=new Node(7);
+    System.out.println("Diameter of a Tree: "+diameter(root));
+  }
+}
