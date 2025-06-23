c65ab53edc88730969ed724ccccc25b291fed063
rahul-roy28 <rahulroy36464@gmail.com>
2025-06-23 21:02:54 +0530

    Add recursive method to print nodes at Kth level in a binary tree

---
+++ Java/27.BinaryTree(Part 3)/Kth_level.java
@@ -0,0 +1,41 @@
+public class Kth_level {
+  static class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+      this.data=data;
+      this.left=null;
+      this.right=null;
+    }
+  }
+  public static void kLevel(Node root,int level,int k){
+    if (root==null) {
+      return;
+    }
+    if (level==k) {
+      System.out.print(root.data+" ");
+      return;
+    }
+    kLevel(root.left, level+1, k);
+    kLevel(root.right, level+1, k);
+  }
+  public static void main(String[] args) {
+    /*
+                 1
+                / \
+               2   3
+             /  \ /  \
+            4   56   7
+     */
+    Node root=new Node(1);
+    root.left=new Node(2);
+    root.right=new Node(3);
+    root.left.left=new Node(4);
+    root.left.right=new Node(5);
+    root.right.left=new Node(6);
+    root.right.right=new Node(7);
+    int k=2;
+    kLevel(root, 1, k);
+  }
+}
