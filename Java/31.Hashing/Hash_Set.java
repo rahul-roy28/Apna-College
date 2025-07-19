94defb0f406c0d9cf6807533fd880c0f84df5198
rahul-roy28 <rahulroy36464@gmail.com>
2025-07-19 11:31:22 +0530

    change the input

--- Java/31.Hashing/Hash_Set.java
+++ Java/31.Hashing/Hash_Set.java
@@ -3,10 +3,9 @@ public class Hash_Set {
   public static void main(String[] args) {
     HashSet<Integer> hs=new HashSet<>();
     hs.add(1);
-    hs.add(5);
-    hs.add(1);
-    hs.add(8);
+    hs.add(2);
     hs.add(4);
+    hs.add(2);
     hs.add(1);
     System.out.println(hs);
     System.out.println(hs.size());
