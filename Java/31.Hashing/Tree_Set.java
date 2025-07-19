e831d607051ecd0e57ea168bfe034402646395d0
rahul-roy28 <rahulroy36464@gmail.com>
2025-07-19 12:28:51 +0530

    Use TreeSet to store and print cities in sorted (natural) order

---
+++ Java/31.Hashing/Tree_Set.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Tree_Set {
+  public static void main(String[] args) {
+    TreeSet<String> cities=new TreeSet<>();
+    cities.add("Kolkata");
+    cities.add("Bengaluru");
+    cities.add("Hydrabad");
+    cities.add("Pune");
+    cities.add("Delhi");
+    System.out.println(cities);
+  }
+}
