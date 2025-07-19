4165bf5b47ceac6c2596a8194f3306f00cf68415
rahul-roy28 <rahulroy36464@gmail.com>
2025-07-19 11:23:16 +0530

    Demonstrate basic HashSet operations: add, remove, contains, clear, and size

---
+++ Java/31.Hashing/Hash_Set.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Hash_Set {
+  public static void main(String[] args) {
+    HashSet<Integer> hs=new HashSet<>();
+    hs.add(1);
+    hs.add(5);
+    hs.add(1);
+    hs.add(8);
+    hs.add(4);
+    hs.add(1);
+    System.out.println(hs);
+    System.out.println(hs.size());
+    hs.remove(4);
+    if (hs.contains(4)) {
+      System.out.println("key exists");
+    }else{
+      System.out.println("key does not exists");
+    }
+    hs.clear();
+    System.out.println(hs.size());
+    System.out.println(hs.isEmpty());
+  }
+}
