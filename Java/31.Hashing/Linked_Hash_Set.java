ab692e441fac799a438ef1487e3a58b4734b1c05
rahul-roy28 <rahulroy36464@gmail.com>
2025-07-19 12:21:36 +0530

    Demonstrate LinkedHashSet to maintain insertion order of elements

---
+++ Java/31.Hashing/Linked_Hash_Set.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Linked_Hash_Set {
+  public static void main(String[] args) {
+    LinkedHashSet<String> cities=new LinkedHashSet<>();
+    cities.add("Kolkata");
+    cities.add("Bengaluru");
+    cities.add("Hydrabad");
+    cities.add("Pune");
+    cities.add("Delhi");
+    System.out.println(cities);
+  }
+}
