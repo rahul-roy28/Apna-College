237e4ae85f35975ded8df51b0c60f7dcda19219f
rahul-roy28 <rahulroy36464@gmail.com>
2025-08-20 19:18:58 +0530

    Add UniqueSubstrings program using Trie to count distinct substrings

---
+++ Java/32.Tries/UniqueSubstrings.java
@@ -0,0 +1,43 @@
+public class UniqueSubstrings {
+ static class Node{
+    Node children[]=new Node[26];
+    boolean eow=false;
+    Node(){
+      for(int i=0;i<26;i++){
+        children[i]=null;
+      }
+    }
+  }
+  public static Node root=new Node();
+  public static void insert(String word){
+    Node curr=root;
+    for(int level=0;level<word.length();level++){
+      int idx=word.charAt(level)-'a';
+      if (curr.children[idx]==null) {
+        curr.children[idx]=new Node();
+      }
+      curr=curr.children[idx];
+    }
+    curr.eow=true;
+  }
+  public static int countSubstrings(Node root){
+    if (root==null) {
+      return 0;
+    }
+    int count=0;
+    for(int i=0;i<26;i++){
+      if (root.children[i] !=null) {
+        count +=countSubstrings(root.children[i]);
+      }
+    }
+    return count+1;
+  }
+  public static void main(String[] args) {
+    String str="ababa";
+    for(int i=0;i<str.length();i++){
+      String subStr=str.substring(i);
+      insert(subStr);
+    }
+    System.out.println(countSubstrings(root));
+ } 
+}
