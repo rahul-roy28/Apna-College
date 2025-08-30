c836a346f99dc4a603b280f096bfd0348f34d571
rahul-roy28 <rahulroy36464@gmail.com>
2025-08-30 11:48:01 +0530

    Add tabulation(bottom up) solution for Climbing Stairs problem

--- Java/39.DP(Part 1)/ClimbingStairs.java
+++ Java/39.DP(Part 1)/ClimbingStairs.java
@@ -25,11 +25,22 @@ public class ClimbingStairs {
     dp[n] = memWays(n-1, dp) + memWays(n-2, dp);
     return dp[n];
   }
+  // Tabulation
+  public static int tabWays(int n){
+    int dp[]=new int[n+1];
+    dp[0]=1;
+    dp[1]=1;
+    for(int i=2;i<=n;i++){
+      dp[i]=dp[i-1]+dp[i-2];
+    }
+    return dp[n];
+  }
   public static void main(String[] args) {
     int n=5;
     int dp[]=new int[n+1];
     Arrays.fill(dp, -1);
     System.out.println(ways(n));
     System.out.println(memWays(n, dp));
+    System.out.println(tabWays(n));
   }
 }
