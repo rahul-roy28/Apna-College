6108eccd089aa472edd3ca0e25f0dd0fa62225fe
rahul-roy28 <rahulroy36464@gmail.com>
2025-08-30 11:08:15 +0530

    Add memorization(Top Down) solution for Climbing Stairs problem

--- Java/39.DP(Part 1)/ClimbingStairs.java
+++ Java/39.DP(Part 1)/ClimbingStairs.java
@@ -1,5 +1,7 @@
+import java.lang.reflect.Array;
 import java.util.*;
 public class ClimbingStairs {
+  // Recursion
   public static int ways(int n){
     if (n==0) {
       return 1;
@@ -9,8 +11,25 @@ public class ClimbingStairs {
     }
     return ways(n-1) + ways(n-2);
   }
+  // Memorization
+    public static int memWays(int n, int dp[]){
+    if (n==0) {
+      return 1;
+    }
+    if (n<0) {
+      return 0;
+    }
+    if (dp[n] !=-1) {
+      return dp[n];
+    }
+    dp[n] = memWays(n-1, dp) + memWays(n-2, dp);
+    return dp[n];
+  }
   public static void main(String[] args) {
     int n=5;
+    int dp[]=new int[n+1];
+    Arrays.fill(dp, -1);
     System.out.println(ways(n));
+    System.out.println(memWays(n, dp));
   }
 }
