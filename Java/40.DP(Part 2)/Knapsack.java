fa610c47f978beb4cc5c37f2f22b71f0635cf667
rahul-roy28 <rahulroy36464@gmail.com>
2025-09-01 21:49:39 +0530

    Add Tabulation approach to 0/1 Knapsack problem

--- Java/40.DP(Part 2)/Knapsack.java
+++ Java/40.DP(Part 2)/Knapsack.java
@@ -29,6 +29,41 @@ public class Knapsack {
       return dp[n][w];
     }
   }
+  public static int knapsackTab(int val[], int wt[], int W){
+    int n=val.length;
+    int dp[][]=new int[n+1][W+1];
+    for(int i=0;i<dp.length;i++){
+      dp[i][0]=0;
+    }
+    for(int j=0;j<dp[0].length;j++){
+      dp[0][j]=0;
+    }
+
+    for(int i=1;i<n+1;i++){
+      for(int j=1;j<W+1;j++){
+        int v=val[i-1];
+        int w=wt[i-1];
+        if (w<=j) {
+          int incProfit=v+dp[i-1][j-w];
+          int excProfit=dp[i-1][j];
+          dp[i][j]=Math.max(incProfit, excProfit);
+        }else{
+          int excProfit=dp[i-1][j];
+          dp[i][j]=excProfit;
+        }
+      }
+    }
+    printDP(dp);
+    return dp[n][W];
+  }
+  public static void printDP(int dp[][]){
+    for(int i=0;i<dp.length;i++){
+      for(int j=0;j<dp[0].length;j++){
+        System.out.print(dp[i][j]+" ");
+      }
+      System.out.println();
+    }
+  }
   public static void main(String[] args) {
     int val[]={15,14,10,45,30};
     int wt[]={2,5,1,3,4};
@@ -41,5 +76,6 @@ public class Knapsack {
     }
     System.out.println(knapsackRec(val, wt, w, val.length));
     System.out.println(knapsackMemo(val, wt, w, val.length,dp));
+    System.out.println(knapsackTab(val, wt, w));
   }
 }
