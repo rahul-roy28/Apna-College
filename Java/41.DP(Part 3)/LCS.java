488f7a8a7b86bc7286bf23222446eb51670d88a4
rahul-roy28 <rahulroy36464@gmail.com>
2025-09-10 21:58:34 +0530

    Add LCS problem solution with memoization

--- Java/41.DP(Part 3)/LCS.java
+++ Java/41.DP(Part 3)/LCS.java
@@ -1,15 +1,30 @@
 import java.util.*;
 public class LCS {
-  public static int lcs(String str1, String str2, int n, int m){
+  public static int rec(String str1, String str2, int n, int m){
     if (n==0 || m==0) {
       return 0;
     }
     if (str1.charAt(n-1) == str2.charAt(m-1)) {
-      return lcs(str1, str2, n-1, m-1)+1;
+      return rec(str1, str2, n-1, m-1)+1;
     }else{
-      int ans1=lcs(str1, str2, n-1, m);
-      int ans2=lcs(str1, str2, n, m-1);
+      int ans1=rec(str1, str2, n-1, m);
+      int ans2=rec(str1, str2, n, m-1);
       return Math.max(ans1, ans2);
+    }
+  }
+  public static int memo(String str1, String str2, int n, int m,int dp[][]){
+    if (n==0 || m==0) {
+      return 0;
+    }
+    if (dp[n][m] != -1) {
+      return dp[n][m];
+    }
+    if (str1.charAt(n-1) == str2.charAt(m-1)) {
+      return dp[n][m] = memo(str1, str2, n-1, m-1, dp)+1;
+    }else{
+      int ans1=memo(str1, str2, n-1, m,dp);
+      int ans2=memo(str1, str2, n, m-1,dp);
+      return dp[n][m] = Math.max(ans1, ans2);
     }
   }
   public static void main(String[] args) {
@@ -17,6 +32,14 @@ public class LCS {
     String str2="abdg";
     int n=str1.length();
     int m=str2.length();
-    System.out.println(lcs(str1, str2, n, m));
+    //For memoization
+    int dp[][]=new int[n+1][m+1];
+    for(int i=0;i<n+1;i++){
+      for(int j=0;j<m+1;j++){
+        dp[i][j]=-1;
+      }
+    }
+    System.out.println(rec(str1, str2, n, m));
+    System.out.println(memo(str1, str2, n, m, dp));
   }
 }
