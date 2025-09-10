f5acf346e40a7dba3c54f822d3f9a56c79f90c6c
rahul-roy28 <rahulroy36464@gmail.com>
2025-09-10 20:54:05 +0530

    Add recursive Longest Common Subsequence (LCS) problem solution

---
+++ Java/41.DP(Part 3)/LCS.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class LCS {
+  public static int lcs(String str1, String str2, int n, int m){
+    if (n==0 || m==0) {
+      return 0;
+    }
+    if (str1.charAt(n-1) == str2.charAt(m-1)) {
+      return lcs(str1, str2, n-1, m-1)+1;
+    }else{
+      int ans1=lcs(str1, str2, n-1, m);
+      int ans2=lcs(str1, str2, n, m-1);
+      return Math.max(ans1, ans2);
+    }
+  }
+  public static void main(String[] args) {
+    String str1="abcdge";
+    String str2="abdg";
+    int n=str1.length();
+    int m=str2.length();
+    System.out.println(lcs(str1, str2, n, m));
+  }
+}
