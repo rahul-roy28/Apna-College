4f5bc1dfc086f0a88446722ac90a86e9e725b2b6
rahul-roy28 <rahulroy36464@gmail.com>
2025-09-11 19:13:38 +0530

    Add LCS problem solution with tabulation

--- Java/41.DP(Part 3)/LCS.java
+++ Java/41.DP(Part 3)/LCS.java
@@ -27,6 +27,30 @@ public class LCS {
       return dp[n][m] = Math.max(ans1, ans2);
     }
   }
+  public static int tab(String text1, String text2) {
+    int n=text1.length();
+        int m=text2.length();
+        //Tabulation
+        int dp[][]=new int[n+1][m+1];
+        for(int i=0;i<dp.length;i++){
+            dp[i][0]=0;
+        }
+        for(int j=0;j<dp[0].length;j++){
+            dp[0][j]=0;
+        }
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }else{
+                    int ans1=dp[i-1][j];
+                    int ans2=dp[i][j-1];
+                    dp[i][j]=Math.max(ans1,ans2);
+                }
+            }
+        }
+        return dp[n][m];      
+  }      
   public static void main(String[] args) {
     String str1="abcdge";
     String str2="abdg";
@@ -41,5 +65,6 @@ public class LCS {
     }
     System.out.println(rec(str1, str2, n, m));
     System.out.println(memo(str1, str2, n, m, dp));
+    System.out.println(tab(str1, str2));
   }
 }
