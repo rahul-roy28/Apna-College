2c7667871cb46acd93d1e16992d269cdc341cfe8
rahul-roy28 <rahulroy36464@gmail.com>
2025-09-18 20:01:57 +0530

    Add CatalanNumber implementation with memoization

--- Java/43.DP(Part 5)/CatalanNumber.java
+++ Java/43.DP(Part 5)/CatalanNumber.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class CatalanNumber {
   // Recursion
   public static int cNumRec(int n){
@@ -10,8 +12,24 @@ public class CatalanNumber {
     }
     return ans;
   }
+  public static int cNumMem(int n, int[] dp){
+    if (n==0 || n==1) {
+      return 1;
+    }
+    if (dp[n] != -1) {
+      return dp[n];
+    }
+    int ans =0;
+    for(int i=0;i<=n-1;i++){
+      ans +=cNumMem(i, dp) * cNumMem(n-1-i, dp);
+    }
+    return dp[n]=ans;
+  }
   public static void main(String[] args) {
-    int n=4;
-    System.out.print(cNumRec(n));
+    int n=40;
+    // System.out.println(cNumRec(n));
+    int dp[]=new int[n+1];
+    Arrays.fill(dp, -1);
+    System.out.print(cNumMem(n, dp));
   }
 }
