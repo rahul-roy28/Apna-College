12dc363de885a469909dc1d63affe780875475d2
rahul-roy28 <rahulroy36464@gmail.com>
2025-09-18 20:14:09 +0530

    Add CatalanNumber implementation with tabulation

--- Java/43.DP(Part 5)/CatalanNumber.java
+++ Java/43.DP(Part 5)/CatalanNumber.java
@@ -12,6 +12,7 @@ public class CatalanNumber {
     }
     return ans;
   }
+  // Memoization
   public static int cNumMem(int n, int[] dp){
     if (n==0 || n==1) {
       return 1;
@@ -25,11 +26,22 @@ public class CatalanNumber {
     }
     return dp[n]=ans;
   }
+  public static int cNumTab(int n){
+    int dp[]=new int[n+1];
+    dp[0]=dp[1]=1;
+    for(int i=2;i<n+1;i++){
+      for(int j=0;j<=i-1;j++){
+        dp[i] +=dp[j]*dp[i-1-j];
+      }
+    }
+    return dp[n];
+  }
   public static void main(String[] args) {
     int n=40;
     // System.out.println(cNumRec(n));
     int dp[]=new int[n+1];
     Arrays.fill(dp, -1);
-    System.out.print(cNumMem(n, dp));
+    System.out.println(cNumMem(n, dp));
+    System.out.print(cNumTab(n));
   }
 }
