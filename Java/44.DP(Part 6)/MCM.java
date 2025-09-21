75ef6c523c64b87f8ca175d39a7eaee8f599edaa
rahul-roy28 <rahulroy36464@gmail.com>
2025-09-21 13:02:52 +0530

    implement tabulation approaches for Matrix Chain Multiplication (MCM)

--- Java/44.DP(Part 6)/MCM.java
+++ Java/44.DP(Part 6)/MCM.java
@@ -32,6 +32,28 @@ public class MCM {
     }
     return dp[i][j]=ans;
   }
+  public static int mcmTab(int arr[]){
+    int n=arr.length;
+    int dp[][]=new int[n][n];
+    
+    for(int i=0;i<n;i++){
+      dp[i][i]=0;
+    }
+
+    for(int len=2;len<=n-1;len++){
+      for(int i=1;i<=n-len;i++){
+        int j=i+len-1;
+        dp[i][j]=Integer.MAX_VALUE;
+        for(int k=i;k<=j-1;k++){
+          int cost1=dp[i][k];
+          int cost2=dp[k+1][j];
+          int cost3=arr[i-1]*arr[k]*arr[j];
+          dp[i][j]=Math.min(dp[i][j], cost1+cost2+cost3);
+        }
+      }
+    }
+    return dp[1][n-1];
+  }
   public static void main(String[] args) {
     int arr[]={1,2,3,4,3};
     int n=arr.length;
@@ -41,5 +63,6 @@ public class MCM {
     }
     System.out.println(mcmRec(arr, 1, n-1));
     System.out.println(mcmMem(arr, 1, n-1, dp));
+    System.out.println(mcmTab(arr));
   }
 }
