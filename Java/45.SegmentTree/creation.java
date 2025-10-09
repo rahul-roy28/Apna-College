59b61a78bc806741a490b82e5f07307a5bd33123
rahul-roy28 <rahulroy36464@gmail.com>
2025-10-09 23:20:09 +0530

    add range sum query (getSum) functionality to segment tree implementation

--- Java/45.SegmentTree/creation.java
+++ Java/45.SegmentTree/creation.java
@@ -14,6 +14,22 @@ public class creation {
     tree[i]=tree[2*i+1]+tree[2*i+2];
     return tree[i];
   }
+  public static int getSumUtil(int i, int si, int sj, int qi, int qj){
+    if (qj<=si || qi>=sj) {
+      return 0;
+    }else if (si>=qi && sj<=qj) {
+      return tree[i];
+    }else{
+      int mid=(si+sj)/2;
+      int left=getSumUtil(2*i+1, si, mid, qi, qj);
+      int right=getSumUtil(2*i+2, mid+1, sj, qi, qj);
+      return left+right;
+    }
+  }
+  public static int getSum(int arr[], int qi, int qj){
+    int n=arr.length;
+    return getSumUtil(0, 0, n-1, qi, qj);
+  }
   public static void main(String[] args) {
     int arr[]={1,2,3,4,5,6,7,8};
     int n=arr.length;
@@ -22,5 +38,6 @@ public class creation {
     for(int i=0;i<tree.length;i++){
       System.out.print(tree[i]+" ");
     }
+    System.out.println(getSum(arr, 2, 5));
   }
 }
