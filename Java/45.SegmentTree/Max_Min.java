d1377bc58d807372a2065f0973d79f12d64bd1d2
rahul-roy28 <rahulroy36464@gmail.com>
2025-10-10 17:43:33 +0530

    implement segment tree for range maximum query with point update functionality

--- Java/45.SegmentTree/Max_Min.java
+++ Java/45.SegmentTree/Max_Min.java
@@ -13,6 +13,42 @@ public class Max_Min {
     build(arr, 2*i+2, mid+1, sj);
     tree[i]=Math.max(tree[2*i+1], tree[2*i+2]);
   }
+  public static int getMaxUtil(int i, int si, int sj, int qi, int qj){
+    if (qj<si || qi>sj) {
+      return Integer.MIN_VALUE;
+    }else if (si>=qi && sj<=qj) {
+      return tree[i];
+    }else{
+      int mid=(si+sj)/2;
+      int left=getMaxUtil(2*i+1, si, mid, qi, qj);
+      int right=getMaxUtil(2*i+2, mid+1, sj, qi, qj);
+      return Math.max(left, right);
+    }
+  }
+  public static int getMax(int arr[], int qi, int qj){
+    int n=arr.length;
+    return getMaxUtil(0, 0, n-1, qi, qj);
+  }
+  public static void updateUtil(int i, int si, int sj, int idx, int newVal){
+    if (si>idx || sj<idx) {
+      return;
+    }
+    if (si==sj) {
+      tree[i]=newVal;
+      return;
+    }
+    if (si != sj) {
+      tree[i]=Math.max(tree[i], newVal);
+      int mid=(si+sj)/2;
+      updateUtil(2*i+1, si, mid, idx, newVal);
+      updateUtil(2*i+2, mid+1, sj, idx, newVal);
+    }
+  }
+  public static void update(int arr[], int idx, int newVal){
+    int n=arr.length;
+    arr[idx]=newVal;
+    updateUtil(0, 0, n-1, idx, newVal);
+  }
   public static void main(String[] args) {
     int arr[]={6,8,-1,2,17,1,3,2,4};
     int n=arr.length;
@@ -21,5 +57,9 @@ public class Max_Min {
     for(int i=0;i<tree.length;i++){
       System.out.print(tree[i]+" ");
     }
+    System.out.println();
+    System.out.println(getMax(arr, 2, 5));
+    update(arr, 2, 20);
+    System.out.println(getMax(arr, 2, 5));
   }
 }
