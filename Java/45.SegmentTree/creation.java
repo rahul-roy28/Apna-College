9645b4d6c9ce0c616161641abd8641530df2c97a
rahul-roy28 <rahulroy36464@gmail.com>
2025-10-10 10:00:43 +0530

    implement point update operation in segment tree with dynamic sum recalculation

--- Java/45.SegmentTree/creation.java
+++ Java/45.SegmentTree/creation.java
@@ -30,6 +30,23 @@ public class creation {
     int n=arr.length;
     return getSumUtil(0, 0, n-1, qi, qj);
   }
+  public static void updateUtil(int i, int si, int sj, int idx, int diff){
+    if (si>idx || sj<idx) {
+      return;
+    }
+    tree[i] +=diff;
+    if (si != sj) {
+      int mid=(si+sj)/2;
+      updateUtil(2*i+1, si, mid, idx, diff);
+      updateUtil(2*i+2, mid+1, sj, idx, diff);
+    }
+  }
+  public static void update(int arr[], int idx, int newVal){
+    int n=arr.length;
+    int diff=newVal-arr[idx];
+    arr[idx]=newVal;
+    updateUtil(0, 0, n-1, idx, diff);
+  }
   public static void main(String[] args) {
     int arr[]={1,2,3,4,5,6,7,8};
     int n=arr.length;
@@ -38,6 +55,9 @@ public class creation {
     for(int i=0;i<tree.length;i++){
       System.out.print(tree[i]+" ");
     }
+    System.out.println();
+    System.out.println(getSum(arr, 2, 5));
+    update(arr, 2, 2);
     System.out.println(getSum(arr, 2, 5));
   }
 }
